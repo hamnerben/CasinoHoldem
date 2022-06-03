4f339e3a5025964607940c195aaf156e44f9864e
hamnerben <hamnerben@gmail.com>
2022-06-03 11:13:03 -0600

    keep at the check methods! flush wordsk

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -1,8 +1,21 @@
 import Deck
 import Hand
 
-
+def runTests():
+    dk = Deck.Deck()
+    for i in range(50):
+        dk.shuffle()
+        p1Hand = Hand.Hand(dk[1, 2, 3, 4, 5])
+        bigHand = dk[1, 2, 3, 4, 5, 6, 7, 8]
+        p1Hand.checkFlush(bigHand)
+        if p1Hand.flush:
+            print("!!!!!!!!!!")
+            print("!!!!!!!!!!")
+            print("!!!!!!!!!!")
+        print("--BIG HAND--")
+        print(p1Hand.getNames(bigHand))
+        p1Hand.state()
 
 if __name__ == '__main__':
-    dk = Deck.Deck()
-    print(dk)
+
+    runTests()
